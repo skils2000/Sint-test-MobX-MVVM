006744b93a0817b2ef1ecaf7693abc560ffd64e4
Ilya Skorokhodov <skorox2000@gmail.com>
2024-07-10 17:37:22 +0300

    Formated and udated ButtonControl

--- src/components/ButtonControl.tsx
+++ src/components/ButtonControl.tsx
@@ -3,36 +3,40 @@ import { observer } from "mobx-react-lite";
 import ControlStore from "../store/ControlStore";
 
 interface ButtonProps {
-    text: string;
-    onClick: () => void;
+  text: string;
+  onClick: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
-    <button onClick={onClick}>{text}</button>
+  <button onClick={onClick}>{text}</button>
 );
 
 interface ButtonControlProps {
-    rightButtons: ButtonProps[];
-    leftButtons?: ButtonProps[];
-    store: ControlStore;
+  rightButtons?: ButtonProps[];
+  leftButtons?: ButtonProps[];
+  store: ControlStore;
 }
 
-const ButtonControl: React.FC<ButtonControlProps> = ({ rightButtons, leftButtons = [], store }) => {
-    return (
-        <div>
-            {leftButtons.map((btn, index) => (
-                <Button key={index} {...btn} />
-            ))}
-            <input
-                type="text"
-                value={store.text}
-                onChange={(e) => store.setText(e.target.value)}
-            />
-            {rightButtons.map((btn, index) => (
-                <Button key={index} {...btn} />
-            ))}
-        </div>
-    );
+const ButtonControl: React.FC<ButtonControlProps> = ({
+  rightButtons = [],
+  leftButtons = [],
+  store,
+}) => {
+  return (
+    <div>
+      {leftButtons.map((btn, index) => (
+        <Button key={index} {...btn} />
+      ))}
+      <input
+        type="text"
+        value={store.text}
+        onChange={(e) => store.setText(e.target.value)}
+      />
+      {rightButtons.map((btn, index) => (
+        <Button key={index} {...btn} />
+      ))}
+    </div>
+  );
 };
 
 export default observer(ButtonControl);

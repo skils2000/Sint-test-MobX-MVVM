29d7fac49e28331356801fe5f2f9278634e237a0
Ilya Skorokhodov <skorox2000@gmail.com>
2024-07-10 17:56:00 +0300

    Added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -16,36 +16,62 @@ autocompleteStore2.setMaxSuggestions(10);
 
 function App() {
   return (
-    <div>
-      <ButtonControl
-        rightButtons={[
-          { text: "Clear", onClick: () => controlStore1.clearText() },
-          {
-            text: "Hello World",
-            onClick: () => controlStore1.setText("Hello world!"),
-          },
-        ]}
-        store={controlStore1}
-      />
-      <ButtonControl
-        leftButtons={[
-          {
-            text: "Alert Number",
-            onClick: () => {
-              if (!isNaN(Number(controlStore2.text))) {
-                alert(controlStore2.text);
-              }
-            },
-          },
-        ]}
-        rightButtons={[
-          { text: "Alert Text", onClick: () => alert(controlStore2.text) },
-        ]}
-        store={controlStore2}
-      />
-      <AutocompleteControl store={autocompleteStore1} />
-      <AutocompleteControl store={autocompleteStore2} />
-    </div>
+    <main>
+      <h1>Тестовое задание</h1>
+      <section>
+        <h2>Контрол с кнопками</h2>
+        <article className="buttonControlsArticle">
+          <div className="example">
+            <p className="margin-0">Пример 1:</p>
+            <ButtonControl
+              rightButtons={[
+                { text: "Очистить", onClick: () => controlStore1.clearText() },
+                {
+                  text: "Hello World",
+                  onClick: () => controlStore1.setText("Hello world!"),
+                },
+              ]}
+              store={controlStore1}
+            />
+          </div>
+          <div className="example">
+            <p className="margin-0">Пример 2:</p>
+            <ButtonControl
+              leftButtons={[
+                {
+                  text: "Alert Число",
+                  onClick: () => {
+                    if (!isNaN(Number(controlStore2.text))) {
+                      alert(controlStore2.text);
+                    }
+                  },
+                },
+              ]}
+              rightButtons={[
+                {
+                  text: "Alert Текст",
+                  onClick: () => alert(controlStore2.text),
+                },
+              ]}
+              store={controlStore2}
+            />
+          </div>
+        </article>
+      </section>
+      <section>
+        <h2>Контрол-автокомплит</h2>
+        <article className="buttonControlsArticle">
+          <div className="example">
+            <p className="margin-0">Пример 1 (3 подсказки):</p>
+            <AutocompleteControl store={autocompleteStore1} />
+          </div>
+          <div className="example">
+            <p className="margin-0">Пример 2 (10 подсказок):</p>
+            <AutocompleteControl store={autocompleteStore2} />
+          </div>
+        </article>
+      </section>
+    </main>
   );
 }
 
